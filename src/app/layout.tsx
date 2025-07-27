dabc0e61a0bac72ea0b5df56b6e56fd49e2f3d97
Divyansh Yadav <anshmcs@gmail.com>
2025-07-27 11:22:31 +0530

    bottom navigation ui in mobile fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <link rel="icon" href="/vercel.svg" type="image/svg+xml" />
         <title>Destyn - Dil se Date tak...</title>
       </head>
